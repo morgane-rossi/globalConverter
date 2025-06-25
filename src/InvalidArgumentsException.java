5f14dfc3342c2c7a13ce0f4fb494a82a8f0b39e8
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-06-25 11:16:19 +0200

    feat: add exception for flags system

---
+++ src/InvalidArgumentsException.java
@@ -0,0 +1,5 @@
+public class InvalidArgumentsException extends Exception {
+    public InvalidArgumentsException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -9,30 +9,40 @@
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            processArgs(args);
+        } catch (InvalidArgumentsException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
-        if ((args.length == 2) || (args.length == 4)) {
-            if (args[2].equals("-k")) {
-                switch (args[0]) {
-                    case "hexadecimal", "-h" -> System.out.println("Hexadecimal : " + args[1] + " Clé : " + args[3]);
-                    case "decimal", "-d" -> System.out.println("Decimal : " + args[1] + " Clé : " + args[3]);
-                    case "octal", "-o" -> System.out.println("Octal : " + args[1] + " Clé : " + args[3]);
-                    case "binary", "-b" -> System.out.println("Binary : " + args[1] + " Clé : " + args[3]);
-                    case "text", "-t" -> System.out.println("Text : " + args[1] + " Clé : " + args[3]);
-                }
-            } else {
-                switch (args[0]) {
-                    case "hexadecimal", "-h" -> System.out.println("Hexadecimal : " + args[1]);
-                    case "decimal", "-d" -> System.out.println("Decimal : " + args[1]);
-                    case "octal", "-o" -> System.out.println("Octal : " + args[1]);
-                    case "binary", "-b" -> System.out.println("Binary : " + args[1]);
-                    case "text", "-t" -> System.out.println("Text : " + args[1]);
-                }
+    public static void processArgs(String[] args) throws InvalidArgumentsException {
+        if (args.length == 2) {
+            // Mode without key
+            switch (args[0]) {
+                case "hexadecimal", "-h" -> System.out.println("Hexadecimal: " + args[1]);
+                case "decimal", "-d" -> System.out.println("Decimal: " + args[1]);
+                case "octal", "-o" -> System.out.println("Octal: " + args[1]);
+                case "binary", "-b" -> System.out.println("Binary: " + args[1]);
+                case "text", "-t" -> System.out.println("Text: " + args[1]);
+                default -> throw new InvalidArgumentsException("Unknown format: " + args[0]);
+            }
+        } else if (args.length == 4) {
+            if (!"-k".equals(args[2])) {
+                throw new InvalidArgumentsException("Third argument must be \"-k\".");
+            }
+
+            switch (args[0]) {
+                case "hexadecimal", "-h" -> System.out.println("Hexadecimal: " + args[1] + " Key: " + args[3]);
+                case "decimal", "-d" -> System.out.println("Decimal: " + args[1] + " Key: " + args[3]);
+                case "octal", "-o" -> System.out.println("Octal: " + args[1] + " Key: " + args[3]);
+                case "binary", "-b" -> System.out.println("Binary: " + args[1] + " Key: " + args[3]);
+                case "text", "-t" -> System.out.println("Text: " + args[1] + " Key: " + args[3]);
+                default -> throw new InvalidArgumentsException("Unknown format: " + args[0]);
             }
         } else {
-            System.out.println("Nooon !!");
+            throw new InvalidArgumentsException("Number of arguments must be 2 or 4.");
         }
-
-
-
     }
 }
+
