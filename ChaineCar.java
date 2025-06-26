c3298e9ea162744d0ecd5105f55fc7435c8631a5
morgane-du-tweize <morgane_rt@hotmail.com>
2025-06-26 15:02:14 +0200

    ChaineCar class to store base, key and string entry

---
+++ ChaineCar.java
@@ -0,0 +1,20 @@
+package globalConvertor;
+
+public class ChaineCar {
+	int taille;
+	char[] caracteres;
+	String base;
+	int cle;
+	
+	public ChaineCar() {
+	}
+
+	public ChaineCar(int taille, char[] caracteres, String base, int cle) {
+		super();
+		this.taille = taille;
+		this.caracteres = new char[taille];
+		this.base = base;
+		this.cle = cle;
+	}
+
+}
